886f783910b77954d256acf1707599702251a359
pnimmaka <pnimmaka@gmail.com>
2017-09-27 12:29:04 -0700

    Update LightningOutController.js

--- lightning/src/aura/LightningOut/LightningOutController.js
+++ lightning/src/aura/LightningOut/LightningOutController.js
@@ -2,7 +2,7 @@
     openTab : function(component, event, helper) {
         var workspaceAPI = component.find("workspace");
         //var sURL = '#/sObject/' + component.get("v.recordId") + '/view';
-        var sURL = '#/sObject/0016A000003rlmzQAA/view';
+        var sURL = '#/sObject/XXXXX/view';
         console.log('sURL: ' + sURL);
         console.log('workspaceAPI: ' + workspaceAPI);
         workspaceAPI.openTab({
